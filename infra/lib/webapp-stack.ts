d7724273b6f77e19e6b5bc7af3bfd7d8ebd1e01a
Jakob Lund Johannessen <jakob-lj@hotmail.com>
2023-02-20 22:34:53 +0100

    feat: add custom domain

--- infra/lib/webapp-stack.ts
+++ infra/lib/webapp-stack.ts
@@ -1,5 +1,6 @@
 import * as cdk from "aws-cdk-lib";
 import * as s3 from "aws-cdk-lib/aws-s3";
+import * as acm from "aws-cdk-lib/aws-certificatemanager";
 import * as lifligCdk from "@liflig/cdk";
 import * as webappDeploy from "@capraconsulting/webapp-deploy-lambda";
 import { Construct } from "constructs";
@@ -19,7 +20,28 @@ class WebappStack extends cdk.Stack {
       bucketName: "koseprogg-event-webapp-artifacts",
     });
 
-    const webapp = new lifligCdk.webapp.Webapp(this, "KoseproggWebapp", {});
+    /*const cloudFrontCertificate = new acm.Certificate(
+      this,
+      "KoseproggWebappCNAMECertificate",
+      {
+        domainName: "aibattles.koseprogg.no",
+      }
+    );*/
+
+    const cloudFrontCertificate = acm.Certificate.fromCertificateArn(
+      this,
+      "KoseproggAiBattlesCertificate",
+      "arn:aws:acm:us-east-1:859141738257:certificate/c94eba62-1f90-46d5-82d7-b016686efd37"
+    );
+
+    new cdk.CfnOutput(this, "CertArn", {
+      value: cloudFrontCertificate.certificateArn,
+    });
+
+    const webapp = new lifligCdk.webapp.Webapp(this, "KoseproggWebapp", {
+      domainNames: ["aibattles.koseprogg.no"],
+      cloudfrontCertificate: cloudFrontCertificate,
+    });
 
     webapp.addDeployment(
       webappDeploy.Source.bucket(artifactsBucket, "app/latest.zip")
@@ -35,8 +57,8 @@ class WebappStack extends cdk.Stack {
         },
         {
           owner: "koseprogg",
-          repo: "better-tic-tac-toe"
-        }
+          repo: "better-tic-tac-toe",
+        },
       ],
       deploymentBucket: artifactsBucket,
     });
