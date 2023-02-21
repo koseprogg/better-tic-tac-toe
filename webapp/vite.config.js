8c4ee37ed5053c44e235601f3e14707cf6884272
Jakob Lund Johannessen <jakob-lj@hotmail.com>
2023-02-21 17:23:33 +0100

    feat: alias runtime browser

--- webapp/vite.config.js
+++ webapp/vite.config.js
@@ -4,6 +4,7 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  resolve: { alias: { './runtimeConfig': './runtimeConfig.browser' } },
   define: {
     // By default, Vite doesn't include shims for NodeJS/
     // necessary for segment analytics lib to work
